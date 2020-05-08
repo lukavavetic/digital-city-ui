135532c44bb618ab7de5f66664f063541dc37207
Matija Vavetić <47482084+mvavetic@users.noreply.github.com>
2020-05-08 10:51:44 +0200

    Import axios in main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import "./assets/css/tailwind.css"
+import axios from 'axios'
 
 import { router } from './routes.js'
 
 Vue.config.productionTip = false
+Vue.prototype.$http = axios
 
 new Vue({
   router,
