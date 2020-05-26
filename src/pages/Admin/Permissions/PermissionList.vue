5ade8494facbfa81c31abf537e1ee1529809aaf5
Matija Vavetić <47482084+mvavetic@users.noreply.github.com>
2020-05-26 17:57:55 +0200

    Update PermissionList.vue

--- src/pages/Admin/Permissions/PermissionList.vue
+++ src/pages/Admin/Permissions/PermissionList.vue
@@ -1,58 +1,15 @@
 <template>
-   <!-- <div>
-        <div>
-            <div class="flex flex-row">
-                <div class="w-1/2">
-                    <h1 class="content-heading">Dozvole</h1>
-                </div>
-                <div class="w-1/2">
-                    <router-link to="/admin/permissions/add" exact>
-                        <button class="submit-request-button submit-request-button:hover">
-                            Dodaj dozvolu
-                        </button>
-                    </router-link>
-                </div>
-            </div>
-
-            <div class="mt-2">
-                <h1 class="content-subheading">Ovo je pregled dostupnih dozvola.</h1>
-            </div>
-
-            <div class="mt-10">
-                <table class="table">
-                    <thead class="table-head">
-                    <tr>
-                        <th class="th text-left">Ime dozvole</th>
-                        <th colspan="1"></th>
-                    </tr>
-                    </thead>
-                    <tbody class="table-body">
-                    <tr class="tr tr:hover" v-for="permission in permissions" v-bind:key="permission.identifier">
-                        <td class="td text-left">
-                            <router-link :to="'/admin/permissions/edit/' + permission.identifier">
-                            {{ permission.name }}
-                            </router-link>
-                        </td>
-                        <td class="td">
-                            <i class="far fa-eye fa-lg hover:text-blue-800 cursor-pointer"></i>
-                        </td>
-                        <td class="td text-left">
-                            <button v-on:click.prevent="destroy(permission.identifier)" class="form-submit-button form-submit-button:hover">x</button>
-                        </td>
-                    </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div> -->
-
     <div>
         <div class="flex flex-row">
             <div class="w-1/2">
                 <h1 class="content-heading">Dozvole</h1>
             </div>
             <div class="w-1/2">
-                <button class="submit-request-button submit-request-button:hover">Dodaj dozvolu</button>
+                <router-link to="/admin/permissions/add" exact>
+                    <button class="submit-request-button submit-request-button:hover">
+                        Dodaj dozvolu
+                    </button>
+                </router-link>
             </div>
         </div>
 
@@ -75,10 +32,10 @@
                         <i class="far fa-eye fa-lg hover:text-blue-800 cursor-pointer"></i>
                     </td>
                     <td class="td">
-                        <i class="fas fa-pen fa-lg hover:text-blue-800 cursor-pointer"></i>
+                        <i class="fas fa-pen fa-lg hover:text-blue-800 cursor-pointer" ><router-link :to="'/admin/permissions/edit/' + permission.identifier"></router-link></i>
                     </td>
                     <td class="td">
-                        <i class="fas fa-trash fa-lg text-red-600 hover:text-red-700 cursor-pointer"></i>
+                        <i class="fas fa-trash fa-lg text-red-600 hover:text-red-700 cursor-pointer" v-on:click.prevent="destroy(permission.identifier)"></i>
                     </td>
                 </tr>
                 </tbody>
